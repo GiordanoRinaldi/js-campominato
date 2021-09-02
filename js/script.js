eed060addebd0e3b38a08558806376ecd44ea2c5
GiordanoRinaldi <giordanorinaldi0@gmail.com>
2021-09-02 17:18:46 +0200

    aggiunte difficoltá + "pulizia" js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,8 @@
-//Chiedere all'utente di inserire il numero di celle di cui sarà composto il campo da gioco.
+/**
+ * FUNZIONI
+ */
 
-var celleUtente = parseInt(prompt("Di quante celle vuoi che sia il campo ?"));
-
-while(isNaN(celleUtente) || celleUtente < 1) {
-    celleUtente = parseInt(prompt("ERRORE! Hai inserito una parola o un numero inferiore a 1. Di quante celle vuoi che sia il campo ?"));
-}
-
-var numerocelle = []; 
-
-//Tramite una funzione javascript disegnare in pagina la griglia con massimo 10 celle per riga.
+// CREA LA DIMENZIONE DEL CAMPO
 function creaCampo(nCelle) {
     for (var i = 1; i <= nCelle; i++) {
     document.getElementById("container").innerHTML += `<div class="quadrato">${i}</div>`;
@@ -16,38 +10,37 @@ function creaCampo(nCelle) {
     }    
 }
 
-creaCampo(celleUtente);
 
-
-// funzione restituisce true se l'elemento é presente 
-function inArray(arr, el) {
-
-    var cont = 0;
-
-    while ( cont < arr.lenght) {
-        if ( arr[cont] == el) {
-            return true;
-        }
-
-        cont++
-    }
-
-    return false;
-}
-
-
-//Consegna
-//Il computer deve generare 16 numeri casuali tra 1 e 100 (bombe).
-//I numeri non possono essere duplicati.
-var bombe = [ ];
-
+// ESEGUE NUMERI RANDOM
 function numeroRandom(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
 
-var classeQuadrato = document.getElementsByClassName("quadrato");
 
-console.log(classeQuadrato[0])
+//Chiedere all'utente di inserire la difficolta di cui sarà composto il campo da gioco.
+
+var sceltaUtente = prompt("Scegli la difficoltá: Facile (100 celle / 16 bombe), Medio (80 celle / 16 bombe), Difficile (50 celle / 16 bombe).");
+
+while(sceltaUtente != "facile" && sceltaUtente != "medio" && sceltaUtente != "difficile") {
+    sceltaUtente = prompt("ERRORE! Non hai inserito i termini giusti. Scegli la difficoltá: Facile (100 celle / 16 bombe), Medio (80 celle / 16 bombe), Difficile (50 celle / 16 bombe).");
+}
+
+if (sceltaUtente == "facile") {
+    sceltaUtente = 100;
+    vittoria = 84;
+} else if (sceltaUtente == "medio") {
+    sceltaUtente = 80;
+    vittoria = 64
+} else {
+    sceltaUtente = 50;
+    vittoria = 34;
+}
+
+ 
+creaCampo(sceltaUtente);
+
+
+// genera bombe random sul campo 
 
 for (var i = 0; i < 16; i++) {
     var numeriBombe = (numeroRandom(1, 100));
@@ -68,6 +61,11 @@ for (var i = 0; i < 16; i++) {
 }
 
 
+var numerocelle = [];
+
+var classeQuadrato = document.getElementsByClassName("quadrato");
+
+
 for (var i = 0; i <= 100; i++) {
     if (bombe.includes(numerocelle[i])) {
         classeQuadrato[i].classList.add("bomba");
@@ -75,10 +73,7 @@ for (var i = 0; i <= 100; i++) {
 }
 
 
-
-
-//In seguito il giocatore clicca sulle celle numerate (non può cliccare più volte sulla stessa cella)
-//Al click su una cella dovrà essere mostrato con un alert il numero della cella e il suo background diventerà rosso.
+// contatore di celle cliccate, bombe cliccate, vittoria 
 
 var celleCliccate = 0;
 
@@ -94,7 +89,7 @@ document.getElementById("container").addEventListener("click",
             alert("CHICCO CHAI GIÁ PREMUTO !");
         } else {
             event.target.classList.add("rosso");
-            //alert(event.target.innerHTML);
+            alert(event.target.innerHTML);
             celleCliccate += 1;
             
             if(celleCliccate == vittoria) {
@@ -105,20 +100,6 @@ document.getElementById("container").addEventListener("click",
 );
 
 
-
-
-
-
-
-
-
-//La partita termina quando il giocatore clicca su un numero “vietato” o clicca su tutte le celle che non sono delle bombe.
-//Al termine della partita il software deve comunicare il punteggio.
-//BONUS: (da fare solo se funziona tutto il resto)
-//all’inizio il software richiede anche una difficoltà all’utente che cambia il range di numeri casuali:
-//con difficoltà 0 => tra 1 e 100
-//con difficoltà 1 => tra 1 e 80
-//con difficoltà 2 => tra 1 e 50
 
 
 
