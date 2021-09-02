258b6ec70df9aa29bd6d6dad61b8ebc787bf86e7
GiordanoRinaldi <giordanorinaldi0@gmail.com>
2021-09-02 16:19:48 +0200

    agg. funz. boom or giá premuto

--- js/script.js
+++ js/script.js
@@ -6,28 +6,20 @@ while(isNaN(celleUtente) || celleUtente < 1) {
     celleUtente = parseInt(prompt("ERRORE! Hai inserito una parola o un numero inferiore a 1. Di quante celle vuoi che sia il campo ?"));
 }
 
+var numerocelle = []; 
+
 //Tramite una funzione javascript disegnare in pagina la griglia con massimo 10 celle per riga.
 function creaCampo(nCelle) {
     for (var i = 1; i <= nCelle; i++) {
     document.getElementById("container").innerHTML += `<div class="quadrato">${i}</div>`;
+    numerocelle.push(i);
     }    
 }
 
 creaCampo(celleUtente);
 
 
-//Al click su una cella dovrà essere mostrato con un alert il numero della cella e il suo background diventerà rosso.
-
-document.getElementById("container").addEventListener("click", 
-    function(event) {
-        event.target.classList.toggle("rosso");
-        alert(event.target.innerHTML);
-    }
-);
-
-
 // funzione restituisce true se l'elemento é presente 
-
 function inArray(arr, el) {
 
     var cont = 0;
@@ -53,6 +45,9 @@ function numeroRandom(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
 
+var classeQuadrato = document.getElementsByClassName("quadrato");
+
+console.log(classeQuadrato[0])
 
 for (var i = 0; i < 16; i++) {
     var numeriBombe = (numeroRandom(1, 100));
@@ -60,23 +55,65 @@ for (var i = 0; i < 16; i++) {
 
     if (check == false) {
         bombe.push(numeriBombe);
+
     } else {
         while(check == true){
             numeriBombe = (numeroRandom(1, 100));
             check = bombe.includes(numeriBombe);
             if(check == false){
-                bombe.push(numeriBombe)
+                bombe.push(numeriBombe);
             }
         }
     }
 }
 
-console.log(bombe);
+
+for (var i = 0; i <= 100; i++) {
+    if (bombe.includes(numerocelle[i])) {
+        classeQuadrato[i].classList.add("bomba");
+    }
+}
 
 
 
 
 //In seguito il giocatore clicca sulle celle numerate (non può cliccare più volte sulla stessa cella)
+//Al click su una cella dovrà essere mostrato con un alert il numero della cella e il suo background diventerà rosso.
+
+
+document.getElementById("container").addEventListener("click", 
+    function (event) {
+        if (event.target.classList.contains("bomba")){
+            event.target.classList.add("boom")
+            alert("BOOM! HAI PERSO!");
+        } else if (event.target.classList.contains("rosso")) {
+            alert("CHICCO CHAI GIÁ PREMUTO !");
+        } else {
+            event.target.classList.add("rosso");
+            alert(event.target.innerHTML);
+        }
+    }
+);
+
+
+//} else if (event.target.classList.contains("rosso") {
+//    alert("BOOM! HAI PERSO!");
+//} else {
+//    event.target.classList.add("rosso");
+//    alert(event.target.innerHTML);
+//}
+
+//document.getElementById("container").addEventListener("click", 
+//    function (event) {
+//        event.target.classList.contains("rosso");
+//        alert("CHICCO CHAI GIÁ PREMUTO !");
+//    }
+//);
+
+
+
+
+
 //La partita termina quando il giocatore clicca su un numero “vietato” o clicca su tutte le celle che non sono delle bombe.
 //Al termine della partita il software deve comunicare il punteggio.
 //BONUS: (da fare solo se funziona tutto il resto)
