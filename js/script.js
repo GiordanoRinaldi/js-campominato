9809605c0f9a66819c4ff438fa2c775e5f4a253c
GiordanoRinaldi <giordanorinaldi0@gmail.com>
2021-09-02 11:40:19 +0200

    generatore numeri cas. *16 non duplicati

--- js/script.js
+++ js/script.js
@@ -44,3 +44,48 @@ function inArray(arr, el) {
 }
 
 
+//Consegna
+//Il computer deve generare 16 numeri casuali tra 1 e 100 (bombe).
+//I numeri non possono essere duplicati.
+var bombe = [ ];
+
+function numeroRandom(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+
+for (var i = 0; i < 16; i++) {
+    var numeriBombe = (numeroRandom(1, 100));
+    var check = bombe.includes(numeriBombe);
+
+    if (check == false) {
+        bombe.push(numeriBombe);
+    } else {
+        while(check == true){
+            numeriBombe = (numeroRandom(1, 100));
+            check = bombe.includes(numeriBombe);
+            if(check == false){
+                bombe.push(numeriBombe)
+            }
+        }
+    }
+}
+
+console.log(bombe);
+
+
+
+
+//In seguito il giocatore clicca sulle celle numerate (non può cliccare più volte sulla stessa cella)
+//La partita termina quando il giocatore clicca su un numero “vietato” o clicca su tutte le celle che non sono delle bombe.
+//Al termine della partita il software deve comunicare il punteggio.
+//BONUS: (da fare solo se funziona tutto il resto)
+//all’inizio il software richiede anche una difficoltà all’utente che cambia il range di numeri casuali:
+//con difficoltà 0 => tra 1 e 100
+//con difficoltà 1 => tra 1 e 80
+//con difficoltà 2 => tra 1 e 50
+
+
+
+
+
